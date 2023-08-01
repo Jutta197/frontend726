868b4caf2b211366fed1ac35ee99541bc7aae9d0
Jutta197 <yangzigui99@gmail.com>
2023-08-01 08:27:25 +0800

    js

---
+++ js_ex/dom/code.js
@@ -0,0 +1,5 @@
+let text = document.getElementById("text-1");
+console.log(text);
+console.log(text.innerHTML);
+text.innerHTML = "hello";
+
---
+++ js_ex/practice1/code.js
@@ -0,0 +1,12 @@
+//chrome: ctrl shift j---console
+
+let num = 28;
+console.log(num);
+num = "こにちは";
+console.log(num);
+console.log(5 ** 3);
+
+let age = 30;
+let str = "30";
+console.log(age == str);
+console.log(age === str);
